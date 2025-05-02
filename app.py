c9e735421efbb9ddc5375c4209986701359a6159
AKSHYATA15 <121245715+AKSHYATA15@users.noreply.github.com>
2025-05-02 07:58:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 st.set_page_config(
     page_title="Influencer Discovery",
     layout="wide",
-    initial_sidebar_state="expanded",
+    initial_sidebar_state="collapsed",
     page_icon="🔍"
 )
 
